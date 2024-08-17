0f7bc2cf62b786c9566bea312c60de9c422b167f
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2024-08-18 01:51:04 +0200

    WIP: test: workflow

---
+++ workflow.test.ts
@@ -0,0 +1,27 @@
+import assert from 'node:assert/strict';
+import { resolve } from 'node:path';
+import {
+	type Mock,
+	before,
+	describe,
+	it,
+	mock,
+} from 'node:test';
+
+import type { Api } from "@codemod.com/workflow";
+
+import { workflow } from 'workflow.ts';
+
+
+describe('workflow', () => {
+	const contexts = {
+		getFileContext: () => ({ file: '/tmp/test.ts' }),
+	} as any as Partial<Api['contexts']>;
+
+	it('should', async () => {
+		const result = await workflow({
+			contexts,
+			files,
+		});
+	});
+});
