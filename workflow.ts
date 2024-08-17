ad1d09e5fbe48f30121a2a5c673e507e89b772a4
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2024-08-18 01:50:47 +0200

    fix: update workflow to capture whole import statement

--- workflow.ts
+++ workflow.ts
@@ -8,21 +8,47 @@ export async function workflow({ contexts, files }: Api) {
 		const filepath = contexts.getFileContext().file; // absolute fs path
     await astGrep`rule:
   any:
-  - kind: string_fragment
-    inside:
-      kind: string
-      inside:
-        any:
-        - kind: import_statement
-        - kind: export_statement
-  - kind: string_fragment
-    inside:
-      kind: string
-      inside:
-        kind: arguments
-        inside:
-          kind: call_expression
-          regex: "^require"`
-		.replace(async ({ getNode }) => mapImports(filepath, getNode().text()));
+    - kind: import_statement
+    - kind: export_statement
+      has:
+        kind: string`
+		.replace(async ({ getNode }) => {
+      const statement = getNode();
+      const importSpecifier = statement.find({
+        rule: {
+          kind: "string_fragment",
+          inside: {
+            kind: "string",
+          },
+        },
+      });
+
+      if (!importSpecifier) return;
+
+      const { isType, replacement } = await mapImports(filepath, importSpecifier.text());
+
+      if (!replacement) return;
+
+      const edits = [importSpecifier.replace(replacement)];
+
+      if (
+        isType
+        &&
+        !statement.children().some((node) => node.kind() === 'type')
+      ) {
+        const clause = statement.find({
+          rule: {
+            any: [
+              { kind: 'import_clause' },
+              { kind: 'export_clause' },
+            ],
+          },
+        });
+
+        if (clause) edits[1] = clause.replace(`type ${clause.text()}`);
+      }
+
+      return statement.commitEdits(edits);
+    });
   });
 }
