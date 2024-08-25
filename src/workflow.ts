26ae3bed904a7e7eb7ee013ef001fbf6a75924ae
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2024-08-25 17:22:47 +0200

    fix: correct glob pattern

--- src/workflow.ts
+++ src/workflow.ts
@@ -1,54 +1,64 @@
-import type { Api } from "@codemod.com/workflow";
+import {
+	type Api,
+	api,
+} from '@codemod.com/workflow';
+import type { Helpers } from '@codemod.com/workflow/dist/jsFam.d.ts';
 
 import { mapImports } from './map-imports.ts';
 
 
 export async function workflow({ contexts, files }: Api) {
-	await files("**/*.{cjs,mjs,js,jsx,(d.)?cts,(d.)?mts,(d.)?ts,tsx}").jsFam(async ({ astGrep }) => {
+	await files(globPattern).jsFam(processModule);
+
+	async function processModule({ astGrep }: Helpers) {
 		const filepath = contexts.getFileContext().file; // absolute fs path
-    await astGrep`rule:
+		await astGrep`rule:
   any:
     - kind: import_statement
     - kind: export_statement
       has:
         kind: string`
 		.replace(async ({ getNode }) => {
-      const statement = getNode();
-      const importSpecifier = statement.find({
-        rule: {
-          kind: "string_fragment",
-          inside: {
-            kind: "string",
-          },
-        },
-      });
+			const statement = getNode();
+			const importSpecifier = statement.find({
+				rule: {
+					kind: "string_fragment",
+					inside: {
+						kind: "string",
+					},
+				},
+			});
 
-      if (!importSpecifier) return;
+			if (!importSpecifier) return;
 
-      const { isType, replacement } = await mapImports(filepath, importSpecifier.text());
+			const { isType, replacement } = await mapImports(filepath, importSpecifier.text());
 
-      if (!replacement) return;
+			if (!replacement) return;
 
-      const edits = [importSpecifier.replace(replacement)];
+			const edits = [importSpecifier.replace(replacement)];
 
-      if (
-        isType
-        &&
-        !statement.children().some((node) => node.kind() === 'type')
-      ) {
-        const clause = statement.find({
-          rule: {
-            any: [
-              { kind: 'import_clause' },
-              { kind: 'export_clause' },
-            ],
-          },
-        });
+			if (
+				isType
+				&&
+				!statement.children().some((node) => node.kind() === 'type')
+			) {
+				const clause = statement.find({
+					rule: {
+						any: [
+							{ kind: 'import_clause' },
+							{ kind: 'export_clause' },
+						],
+					},
+				});
 
-        if (clause) edits[1] = clause.replace(`type ${clause.text()}`);
-      }
+				if (clause) edits[1] = clause.replace(`type ${clause.text()}`);
+			}
 
-      return statement.commitEdits(edits);
-    });
-  });
+			return statement.commitEdits(edits);
+		});
+	}
 }
+
+const globPattern = '**/*.{cjs,mjs,js,jsx,?(d.)cts,?(d.)mts,?(d.)ts,tsx}';
+
+workflow(api).then(console.log);
