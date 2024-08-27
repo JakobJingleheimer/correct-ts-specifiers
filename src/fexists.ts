98f0b2002c36b1c82b28c114af3d969dca8e122b
Aleksy <alekso.php@gmail.com>
2024-08-27 10:04:06 +0100

    Fixed tests, changed try-catch to wrap only import.meta.resolve

--- src/fexists.ts
+++ src/fexists.ts
@@ -9,24 +9,30 @@ export function fexists(
 	parentPath: FSPath,
 	specifier: Specifier,
 ) {
-	try {
-	  const parentUrl = `${pathToFileURL(dirname(parentPath)).href}/`;
-  	const resolvedSpecifier = URL.canParse(specifier)
-  		? specifier
-  		// import.meta.resolve here is required because we need node's resolution algorithm to
-  		// incorporate the results of any hooks that may be helping, such as ones facilitating
-  		// tsconfig.compileOptions.paths
-  		: fileURLToPath(import.meta.resolve(specifier, parentUrl));
-  	return access(
-  		resolvedSpecifier,
-  		constants.F_OK,
-  	)
-  	.then(
-  		() => true,
-  		() => false,
-  	);
-	} catch {
-	  return false;
-	}
+	const parentUrl = `${pathToFileURL(dirname(parentPath)).href}/`;
+	
+	let resolvedSpecifier: string;
+	
+  if (URL.canParse(specifier)) {
+    resolvedSpecifier = specifier;
+  } else {
+    // import.meta.resolve here is required because we need node's resolution algorithm to
+		// incorporate the results of any hooks that may be helping, such as ones facilitating
+		// tsconfig.compileOptions.paths
+		// error is thrown if the specifier is not resolvable from `import.meta.resolve`
+		try {
+      resolvedSpecifier = fileURLToPath(import.meta.resolve(specifier, parentUrl));
+		} catch {
+      return false;
+		}
+  }
 
+	return access(
+		resolvedSpecifier,
+		constants.F_OK,
+	)
+	.then(
+		() => true,
+		() => false,
+	);
 };
