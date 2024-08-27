2afa3aa83b157597421367c25e6238ac29b4f0a9
Aleksy <alekso.php@gmail.com>
2024-08-27 09:44:35 +0100

    fixed console.log

--- src/workflow.ts
+++ src/workflow.ts
@@ -33,7 +33,7 @@ export async function workflow({ contexts, files }: Api) {
 			});
 
 			if (!importSpecifier) return;
-      console.log(filepath, importSpecifier.text());
+
 			const { isType, replacement } = await mapImports(filepath, importSpecifier.text());
 
 			if (!replacement) return;
