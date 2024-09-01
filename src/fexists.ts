a0157fdddea97b984d3cb9ded154185fb2fd13ec
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2024-09-01 23:02:50 +0200

    chore: extract `resolveSpecifier` into own module

--- src/fexists.ts
+++ src/fexists.ts
@@ -1,22 +1,14 @@
-import { dirname } from 'node:path';
 import { access, constants } from 'node:fs/promises';
-import { fileURLToPath, pathToFileURL } from 'node:url';
 
 import type { FSAbsolutePath, Specifier } from './index.d.ts';
+import { resolveSpecifier } from './resolveSpecifier.ts';
 
 
 export function fexists(
 	parentPath: FSAbsolutePath,
 	specifier: Specifier,
 ) {
-	const parentUrl = `${pathToFileURL(dirname(parentPath)).href}/`;
-
-	const resolvedSpecifier: FSAbsolutePath = URL.canParse(specifier)
-    ? specifier
-    // import.meta.resolve gives access to node's resolution algorithm, which is necessary to handle
-		// a myriad of non-obvious routes, like pJson subimports and the result of any hooks that may be
-		// helping, such as ones facilitating tsconfig's "paths"
-		: fileURLToPath(import.meta.resolve(specifier, parentUrl));
+	const resolvedSpecifier = resolveSpecifier(parentPath, specifier);
 
 	return access(
 		resolvedSpecifier,
