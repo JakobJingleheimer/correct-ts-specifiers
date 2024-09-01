b5a2e6ba98214ca51ac65be46b535b275c86ea05
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2024-09-01 12:15:53 +0200

    test: add e2e case for `tsconfig`'s `paths` (#4)
    * chore: rename `e2e/e2e.ts` → `e2e/test.ts`
    * fix: `import.meta.resolve` 2nd arg `URL` → `URL['href']`

--- src/workflow.test.ts
+++ src/workflow.test.ts
@@ -13,46 +13,47 @@ describe('workflow', () => {
 		const e2eFixtPath = fileURLToPath(import.meta.resolve('./fixtures/e2e/'));
 
 		await spawnPromisified('node', [
-      '--no-warnings',
-      '--experimental-strip-types',
-      '--experimental-import-meta-resolve',
-      '../../workflow.ts',
-    ], {
+			'--loader=nodejs-loaders/dev/alias',
+			'--no-warnings',
+			'--experimental-strip-types',
+			'--experimental-import-meta-resolve',
+			'../../workflow.ts',
+		], {
 			cwd: e2eFixtPath,
 		});
 
-    const result = await readFile(resolve(e2eFixtPath, 'e2e.ts'), { encoding: 'utf-8' });
+		const result = await readFile(resolve(e2eFixtPath, 'test.ts'), { encoding: 'utf-8' });
 
-    t.assert.snapshot(result);
+		t.assert.snapshot(result);
 	});
 });
 
 function spawnPromisified(...args: Parameters<typeof spawn>) {
-  let stderr = '';
-  let stdout = '';
+	let stderr = '';
+	let stdout = '';
 
-  const child = spawn(...args);
-  child.stderr!.setEncoding('utf8');
-  child.stderr!.on('data', (data) => { stderr += data; });
-  child.stdout!.setEncoding('utf8');
-  child.stdout!.on('data', (data) => { stdout += data; });
+	const child = spawn(...args);
+	child.stderr!.setEncoding('utf8');
+	child.stderr!.on('data', (data) => { stderr += data; });
+	child.stdout!.setEncoding('utf8');
+	child.stdout!.on('data', (data) => { stdout += data; });
 
-  return new Promise((resolve, reject) => {
-    child.on('close', (code, signal) => {
-      resolve({
-        code,
-        signal,
-        stderr,
-        stdout,
-      });
-    });
-    child.on('error', (code, signal) => {
-      reject({
-        code,
-        signal,
-        stderr,
-        stdout,
-      });
-    });
-  });
+	return new Promise((resolve, reject) => {
+		child.on('close', (code, signal) => {
+			resolve({
+				code,
+				signal,
+				stderr,
+				stdout,
+			});
+		});
+		child.on('error', (code, signal) => {
+			reject({
+				code,
+				signal,
+				stderr,
+				stdout,
+			});
+		});
+	});
 }
