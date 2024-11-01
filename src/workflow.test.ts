4a397aa81aea342d9bcf15112e62362ae6b78447
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2024-11-01 23:48:06 +0100

    test: extract `spawnPromisified` into build util

--- src/workflow.test.ts
+++ src/workflow.test.ts
@@ -1,12 +1,13 @@
-import { spawn } from 'node:child_process';
 import { resolve } from 'node:path';
-import { readFile } from 'node:fs/promises';
 import { execPath } from 'node:process';
+import { readFile } from 'node:fs/promises';
 import {
 	describe,
 	it,
 } from 'node:test';
 import { fileURLToPath } from 'node:url';
+
+import { spawnPromisified } from '../build/spawn-promisified.ts';
 
 
 describe('workflow', () => {
@@ -28,33 +29,3 @@ describe('workflow', () => {
 		t.assert.snapshot(result);
 	});
 });
-
-function spawnPromisified(...args: Parameters<typeof spawn>) {
-	let stderr = '';
-	let stdout = '';
-
-	const child = spawn(...args);
-	child.stderr!.setEncoding('utf8');
-	child.stderr!.on('data', (data) => { stderr += data; });
-	child.stdout!.setEncoding('utf8');
-	child.stdout!.on('data', (data) => { stdout += data; });
-
-	return new Promise((resolve, reject) => {
-		child.on('close', (code, signal) => {
-			resolve({
-				code,
-				signal,
-				stderr,
-				stdout,
-			});
-		});
-		child.on('error', (code, signal) => {
-			reject({
-				code,
-				signal,
-				stderr,
-				stdout,
-			});
-		});
-	});
-}
