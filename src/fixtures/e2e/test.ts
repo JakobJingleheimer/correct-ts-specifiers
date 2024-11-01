694b0b5cefa8c913871e0fc882b7ae027216353f
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2024-11-01 19:08:11 +0100

    fix: restore accidentally committed test start
    update snapshot for relocated `makeLink`

--- src/fixtures/e2e/test.ts
+++ src/fixtures/e2e/test.ts
@@ -3,22 +3,22 @@ import { URL } from 'node:url';
 import { bar } from '@dep/bar';
 import { foo } from 'foo';
 
-import { Bird } from './Bird/index.ts';
+import { Bird } from './Bird';
 import { Cat } from './Cat.ts';
-import { Dog } from '…/Dog/index.mts';
+import { Dog } from '…/Dog/index.mjs';
 import { baseUrl } from '#config.js';
 
-export type { Zed } from './zed.d.ts';
+export { Zed } from './zed';
 
 // should be unchanged
 
-export const makeLink = (path: URL) => (new URL(path, baseUrl)).href;
-
-const nil = await import('./nil.ts');
+const nil = await import('./nil.js');
 
 const bird = new Bird('Tweety');
 const cat = new Cat('Milo');
 const dog = new Dog('Otis');
+
+export const makeLink = (path: URL) => (new URL(path, baseUrl)).href;
 
 console.log('bird:', bird);
 console.log('cat:', cat);
