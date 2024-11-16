8cc2e3fc3ab1a91436a570ba81029308aa3469ab
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2024-11-16 13:21:14 +0100

    test: add cases for `getNotFoundUrl` & set types

--- src/get-not-found-url.ts
+++ src/get-not-found-url.ts
@@ -1,6 +1,6 @@
 import { pathToFileURL } from 'node:url';
 
-import type { NodeError } from './index.d.ts';
+import type { NodeError, ResolvedSpecifier } from './index.d.ts';
 
 
-export const getNotFoundUrl = (err: NodeError) => pathToFileURL(err?.url ?? err.message.split('\'')[1])?.href;
+export const getNotFoundUrl = (err: NodeError) => pathToFileURL(err?.url ?? err.message.split('\'')[1])?.href as ResolvedSpecifier;
