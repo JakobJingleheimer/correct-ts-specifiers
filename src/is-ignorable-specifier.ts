884574126457851ee30afb1d15061cbbcf542c8c
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2024-11-20 16:01:04 +0100

    fix: handle node module regardless of whether implementation is found

--- src/is-ignorable-specifier.ts
+++ src/is-ignorable-specifier.ts
@@ -6,8 +6,9 @@ import {
 import { pathToFileURL } from 'node:url';
 
 import { tsExts } from './exts.ts';
-import type { FSAbsolutePath, ResolvedSpecifier } from './index.d.ts';
+import type { FSAbsolutePath, NodeError, ResolvedSpecifier, Specifier } from './index.d.ts';
 import { resolvesToNodeModule } from './resolves-to-node-module.ts';
+import { getNotFoundUrl } from './get-not-found-url.ts';
 
 
 /**
@@ -33,11 +34,18 @@ export function isIgnorableSpecifier(
 	if (specifier.startsWith(`.${sep}`) /* './' */) return false;
 	if (specifier.startsWith('file://')) return false;
 
+	let resolvedSpecifier: ResolvedSpecifier;
 	try {
-		const resolvedSpecifier = import.meta.resolve(specifier, pathToFileURL(parentPath).href) as ResolvedSpecifier; // [1]
-		if (resolvesToNodeModule(resolvedSpecifier, parentPath)) return true;
-	} catch (err) { // @ts-ignore (TS requires type of err to be unknown)
-		if (!IGNORABLE_RESOLVE_ERRORS.has(err?.code)) throw err;
+		resolvedSpecifier = import.meta.resolve(specifier, pathToFileURL(parentPath).href) as ResolvedSpecifier; // [1]
+	} catch (err) {
+		if (
+			!(err instanceof Error)
+			|| !IGNORABLE_RESOLVE_ERRORS.has((err as NodeError).code!)
+		) throw err;
+
+		resolvedSpecifier = getNotFoundUrl(err);
+	} finally {
+		if (resolvesToNodeModule(resolvedSpecifier!, parentPath)) return true;
 	}
 
 	return false;
